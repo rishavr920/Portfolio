f9f28f6fbd0dde03f0b0394a7ce3fb12bb2c6a42
Rishav Raj <rishavr920@gmail.com>
2024-12-21 01:28:47 +0530

    Added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => (
+    <nav className = "flex justify-between items-center p-5 bg-slate-400 text-white">
+        <h1 className="text-3xl font-bold">Portfolio</h1>
+        <ul className = "flex">
+            <li className = "px-4 cursor-pointer ">Home</li>
+            <li className = "px-4 cursor-pointer">About</li>
+        </ul>
+    </nav>
+);
