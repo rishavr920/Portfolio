c926450b6e9edd848911e134fb9c35a08bea0376
Rishav Raj <rishavr920@gmail.com>
2024-12-21 10:14:19 +0530

    Navigation Bar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,54 @@
-export const Navbar = () => (
-    <nav className = "flex justify-between items-center p-5 bg-slate-400 text-white">
-        <h1 className="text-3xl font-bold">Portfolio</h1>
-        <ul className = "flex">
-            <li className = "px-4 cursor-pointer ">Home</li>
-            <li className = "px-4 cursor-pointer">About</li>
-        </ul>
-    </nav>
-);
+import logo from '../assets/rishavLogo.png'
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <a href="/" aria-label="Home">
+                    <img src = {logo} className="mx-2" width={50} height={33} alt="Logo" /> 
+                </a>
+            </div>
+
+            <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            {/* twitter */}
+            
+                <a href='https://x.com/rishav0317'
+                 target="_blank" 
+                 rel="noopener noreferrer"
+                 aria-label='Twitter'>
+                    <FaSquareXTwitter />
+                </a>
+
+            {/* linkedin */}
+                <a href='https://www.linkedin.com/in/rishav-shrivastava-232033184/'
+                 target="_blank" 
+                 rel="noopener noreferrer"
+                 aria-label='LinkedIn'>
+                    <FaLinkedin/>
+                </a>
+
+            {/* instagram */}
+                <a href='https://www.instagram.com/rishavv1712/'
+                 target="_blank" 
+                 rel="noopener noreferrer"
+                 aria-label='Instagram'>
+                    <FaInstagram />
+                </a>
+            
+            {/* github */}
+                <a href='https://www.github.com/in/rishavr920/'
+                 target="_blank" 
+                 rel="noopener noreferrer"
+                 aria-label='GitHub'>
+                    <FaGithub />
+                </a>
+            
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
