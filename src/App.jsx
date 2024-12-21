47ba3f036fe9e665de5714274c849fc14e22f4de
Rishav Raj <rishavr920@gmail.com>
2024-12-21 22:13:40 +0530

    hero-section-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   
@@ -15,6 +16,7 @@ const App = () => {
 
        <div className='container mx-auto px-8'>  
        <Navbar />
+       <Hero/>
        </div>
       </div>
     
